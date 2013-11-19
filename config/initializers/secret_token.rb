7cfb4dd21a4156cd3f951cc3c6fe5120da0108f9
danecjensen <danecjensen@gmail.com>
2013-11-19 16:17:35 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuPdf::Application.config.secret_token = '9acb7bd3a6c6c5856d22de1c5adfc7fa02c0f2f88c8f54f12f5a6acfdf9dcd42a94db77875ad04b6dc6731de7ff2c0222ae08319596adebd57613df7d25d93a9'
